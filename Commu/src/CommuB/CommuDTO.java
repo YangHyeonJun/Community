a7a5c861dec7cd4eb970084f948694e6e2d14dab
YangHyeonJun <tiskqmfls@naver.com>
2019-10-14 15:49:56 +0900

    DTO 생성

---
+++ Commu/src/CommuB/CommuDTO.java
@@ -0,0 +1,41 @@
+package CommuB;
+
+public class CommuDTO {
+
+	private int id;
+	private String writehead;
+	private String writer;
+	private String writeDate;
+	private String writetext;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getWritehead() {
+		return writehead;
+	}
+	public void setWritehead(String writehead) {
+		this.writehead = writehead;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getWritetext() {
+		return writetext;
+	}
+	public void setWritetext(String writetext) {
+		this.writetext = writetext;
+	}
+}
